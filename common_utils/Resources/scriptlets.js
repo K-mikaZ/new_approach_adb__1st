0c817e6253638eead9bb24d55d834c67ce3bfa1b
K-mik@Z <cool2larime@yahoo.fr>
2020-07-12 13:05:25 +0200

    Update scriptlets.js

--- common_utils/Resources/scriptlets.js
+++ common_utils/Resources/scriptlets.js
@@ -10,26 +10,6 @@
 function asyncGeneratorStep(e,r,n,t,a,o,c){try{var u=e[o](c),s=u.value}catch(e){return void n(e)}u.done?r(s):Promise.resolve(s).then(t,a)}function _asyncToGenerator(u){return function(){var e=this,c=arguments;return new Promise(function(r,n){function t(e){asyncGeneratorStep(o,r,n,t,a,"next",e)}function a(e){asyncGeneratorStep(o,r,n,t,a,"throw",e)}var o=u.apply(e,c);t(void 0)})}}var grab=function(){var r=_asyncToGenerator(regeneratorRuntime.mark(function e(r){var n,t;return regeneratorRuntime.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,fetch(r);case 2:if((n=e.sent).ok)return e.next=6,n.text();e.next=10;break;case 6:return t=e.sent,e.abrupt("return",t);case 10:console.log("HTTP-Error: "+n.status);case 11:case"end":return e.stop()}},e)}));return function(e){return r.apply(this,arguments)}}()();
 
 
-//! IN TEST (ADGUARD INTEGRATION)
-// ----------------------------------------------------------------------------------------------------------------- //
-
-// **
-// * AdGuard Scriptlets integration
-// * Version 1.2.5
-// * source metrika-yandex-tag:  https://github.com/AdguardTeam/Scriptlets/blob/master/dist/redirect-files/metrika-yandex-tag.js
-// * source metrika-yandex-watch:  https://github.com/AdguardTeam/Scriptlets/blob/master/dist/redirect-files/metrika-yandex-watch.js
-// */
-
-metrika-yandex-tag application/javascript
-//grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redirect-files/metrika-yandex-tag.js")
-
-metrika-yandex-watch application/javascript
-//grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redirect-files/metrika-yandex-watch.js")
-
-grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redirects.js")
-grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/scriptlets.js")
-
-
 // ----------------------------------------------------------------------------------------------------------------- //
 
 // /**
@@ -50,3 +30,23 @@ grab("https://raw.githubusercontent.com/NanoAdblocker/NanoCore2/master/src/snipp
 // */
 
 grab("https://raw.githubusercontent.com/uBlock-user/uBO-Scriptlets/master/scriptlets.js")
+
+
+//! IN TEST (ADGUARD INTEGRATION)
+// ----------------------------------------------------------------------------------------------------------------- //
+
+// **
+// * AdGuard Scriptlets integration
+// * Version 1.2.5
+// * source metrika-yandex-tag:  https://github.com/AdguardTeam/Scriptlets/blob/master/dist/redirect-files/metrika-yandex-tag.js
+// * source metrika-yandex-watch:  https://github.com/AdguardTeam/Scriptlets/blob/master/dist/redirect-files/metrika-yandex-watch.js
+// */
+
+metrika-yandex-tag application/javascript
+//grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redirect-files/metrika-yandex-tag.js")
+
+metrika-yandex-watch application/javascript
+//grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redirect-files/metrika-yandex-watch.js")
+
+grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redirects.js")
+grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/scriptlets.js")
