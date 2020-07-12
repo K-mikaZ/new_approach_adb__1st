55ec44374f8de3c718216882855ee7e44ecaa321
K-mik@Z <cool2larime@yahoo.fr>
2020-07-12 13:01:05 +0200

    Update scriptlets.js

--- common_utils/Resources/scriptlets.js
+++ common_utils/Resources/scriptlets.js
@@ -10,28 +10,6 @@
 function asyncGeneratorStep(e,r,n,t,a,o,c){try{var u=e[o](c),s=u.value}catch(e){return void n(e)}u.done?r(s):Promise.resolve(s).then(t,a)}function _asyncToGenerator(u){return function(){var e=this,c=arguments;return new Promise(function(r,n){function t(e){asyncGeneratorStep(o,r,n,t,a,"next",e)}function a(e){asyncGeneratorStep(o,r,n,t,a,"throw",e)}var o=u.apply(e,c);t(void 0)})}}var grab=function(){var r=_asyncToGenerator(regeneratorRuntime.mark(function e(r){var n,t;return regeneratorRuntime.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,fetch(r);case 2:if((n=e.sent).ok)return e.next=6,n.text();e.next=10;break;case 6:return t=e.sent,e.abrupt("return",t);case 10:console.log("HTTP-Error: "+n.status);case 11:case"end":return e.stop()}},e)}));return function(e){return r.apply(this,arguments)}}()();
 
 
-// ----------------------------------------------------------------------------------------------------------------- //
-
-// /**
-// * uBlock-user's scriptlet
-// * for the licence, see source:  https://github.com/uBlock-user/uBO-Personal-Filters
-// */
-
-grab("https://raw.githubusercontent.com/uBlock-user/uBO-Scriptlets/master/scriptlets.js")
-
-
-// ----------------------------------------------------------------------------------------------------------------- //
-
-// /**
-// * Nano Core 2 - An adblocker
-// * Copyright (C) 2018-2019  Nano Core 2 contributors
-// * for the licence, see source:  https://github.com/NanoAdblocker/NanoCore2/blob/master/src/snippets.js
-// * For redirectable resources, see: https://github.com/NanoAdblocker/NanoCore2/tree/master/src/war
-// */
-
-grab("https://raw.githubusercontent.com/NanoAdblocker/NanoCore2/master/src/snippets.js")
-
-
 //! IN TEST (ADGUARD INTEGRATION)
 // ----------------------------------------------------------------------------------------------------------------- //
 
@@ -50,3 +28,25 @@ grab("https://raw.githubusercontent.com/NanoAdblocker/NanoCore2/master/src/snipp
 
 grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redirects.js")
 grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/scriptlets.js")
+
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+// /**
+// * Nano Core 2 - An adblocker
+// * Copyright (C) 2018-2019  Nano Core 2 contributors
+// * for the licence, see source:  https://github.com/NanoAdblocker/NanoCore2/blob/master/src/snippets.js
+// * For redirectable resources, see: https://github.com/NanoAdblocker/NanoCore2/tree/master/src/war
+// */
+
+grab("https://raw.githubusercontent.com/NanoAdblocker/NanoCore2/master/src/snippets.js")
+
+
+// ----------------------------------------------------------------------------------------------------------------- //
+
+// /**
+// * uBlock-user's scriptlet
+// * for the licence, see source:  https://github.com/uBlock-user/uBO-Personal-Filters
+// */
+
+grab("https://raw.githubusercontent.com/uBlock-user/uBO-Scriptlets/master/scriptlets.js")
