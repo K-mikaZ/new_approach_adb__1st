53dc1daf4f676c9d11d1bd3c103767656bcbd7d4
K-mik@Z <cool2larime@yahoo.fr>
2020-07-12 13:45:54 +0200

    Update scriptlets.js

--- common_utils/Resources/scriptlets.js
+++ common_utils/Resources/scriptlets.js
@@ -6,9 +6,7 @@
 //  * the "Grabber"
 //  * @author K-mik@Z
 //  */
-
 function asyncGeneratorStep(e,r,n,t,a,o,c){try{var u=e[o](c),s=u.value}catch(e){return void n(e)}u.done?r(s):Promise.resolve(s).then(t,a)}function _asyncToGenerator(u){return function(){var e=this,c=arguments;return new Promise(function(r,n){function t(e){asyncGeneratorStep(o,r,n,t,a,"next",e)}function a(e){asyncGeneratorStep(o,r,n,t,a,"throw",e)}var o=u.apply(e,c);t(void 0)})}}var grab=function(){var r=_asyncToGenerator(regeneratorRuntime.mark(function e(r){var n,t;return regeneratorRuntime.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,fetch(r);case 2:if((n=e.sent).ok)return e.next=6,n.text();e.next=10;break;case 6:return t=e.sent,e.abrupt("return",t);case 10:console.log("HTTP-Error: "+n.status);case 11:case"end":return e.stop()}},e)}));return function(e){return r.apply(this,arguments)}}()();
-
 
 // ----------------------------------------------------------------------------------------------------------------- //
 
@@ -18,14 +16,13 @@ function asyncGeneratorStep(e,r,n,t,a,o,c){try{var u=e[o](c),s=u.value}catch(e){
 // * source metrika-yandex-tag:  https://github.com/AdguardTeam/Scriptlets/blob/master/dist/redirect-files/metrika-yandex-tag.js
 // * source metrika-yandex-watch:  https://github.com/AdguardTeam/Scriptlets/blob/master/dist/redirect-files/metrika-yandex-watch.js
 // */
-
-//grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redirects.js")
+// Utils
 grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/types/scriptlets.d.ts")
 grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/scriptlets.js")
-
+//grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redirects.js")
+// redirect-rule
 metrika-yandex-tag application/javascript
 grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redirect-files/metrika-yandex-tag.js")
-
 metrika-yandex-watch application/javascript
 grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redirect-files/metrika-yandex-watch.js")
 
@@ -37,7 +34,6 @@ grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/redir
 // * for the licence, see source:  https://github.com/NanoAdblocker/NanoCore2/blob/master/src/snippets.js
 // * For redirectable resources, see: https://github.com/NanoAdblocker/NanoCore2/tree/master/src/war
 // */
-
 grab("https://raw.githubusercontent.com/NanoAdblocker/NanoCore2/master/src/snippets.js")
 
 
@@ -47,6 +43,4 @@ grab("https://raw.githubusercontent.com/NanoAdblocker/NanoCore2/master/src/snipp
 // * uBlock-user's scriptlet
 // * for the licence, see source:  https://github.com/uBlock-user/uBO-Personal-Filters
 // */
-
 grab("https://raw.githubusercontent.com/uBlock-user/uBO-Scriptlets/master/scriptlets.js")
-//grab("https://raw.githubusercontent.com/AdguardTeam/Scriptlets/master/dist/scriptlets.js")
